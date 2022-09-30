93604464cbd90dcfd0496a09980dd5916bd690e0
kebin20 <ktanzyl@hotmail.com>
2022-09-30 16:39:03 +0900

    Updated notes in js file

--- app.js
+++ app.js
@@ -1,81 +1,78 @@
 // HTML Components
-const bookContainer = document.querySelector(".book-container")
+const bookContainer = document.querySelector(".book-container");
 
 //Book detail values
-const bookName = document.getElementById("bookName")
-const author = document.getElementById("author")
-const pages = document.getElementById("pages")
-const haveRead = document.getElementById("haveRead")
-
+const bookName = document.getElementById("bookName");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const haveRead = document.getElementById("haveRead");
 
 //Buttons
-const addBookButton = document.querySelector(".add-book-button")
-const saveBookButton = document.querySelector(".save-book-button")
-const bookModal = document.querySelector(".book-modal")
-const clearBookDetails = document.querySelector(".clear-button")
+const addBookButton = document.querySelector(".add-book-button");
+const saveBookButton = document.querySelector(".save-book-button");
+const bookModal = document.querySelector(".book-modal");
+const clearBookDetails = document.querySelector(".clear-button");
 
 //Render the add book screen
-let bookScreenOn = false
+let bookScreenOn = false;
 
-addBookButton.addEventListener("click", renderAddBookScreen)
+addBookButton.addEventListener("click", renderAddBookScreen);
 
 function renderAddBookScreen() {
   bookScreenOn = true;
-  bookModal.classList.remove("hidden")
-  bookModal.classList.add("visible")
+  bookModal.classList.remove("hidden");
+  bookModal.classList.add("visible");
 }
 
-saveBookButton.addEventListener("click",() => {
-   newBook.showBookDataHtml()
-   addBookData()
-   bookScreenOn = false
-    bookModal.classList.remove("visible")
-    bookModal.classList.add("hidden")
-})
+//Render newly inputted books onto screen
+saveBookButton.addEventListener("click", () => {
+  newBook.showBookDataHtml();
+  addBookData();
+  bookScreenOn = false;
+  bookModal.classList.remove("visible");
+  bookModal.classList.add("hidden");
+});
 
-//CREATE A LIBRARY CLASS AND FILL OUT BOOK CONSTRUCTOR
 //Push new inputed books into bookData array
 function addBookData() {
-   bookData.push({
+  bookData.push({
     name: bookName.value,
     author: author.value,
-    pages:pages.value,
-    read: haveRead.value
-  })
-  console.log(bookData)
+    pages: pages.value,
+    read: haveRead.value,
+  });
+  console.log(bookData);
 }
 
 //Data array
-let bookData = []
+let bookData = [];
 
 //Constructor
 class Book {
   constructor(data) {
-   Object.assign(this,data)
-  } 
-  
-    showBookDataHtml(data) {
-    const {name, author, pages, read} = this;
-    return bookContainer.innerHTML = `
+    Object.assign(this, data);
+  }
+
+  showBookDataHtml(data) {
+    const { name, author, pages, read } = this;
+    return (bookContainer.innerHTML = `
        <div class="book" id="showBook">
        <label for="bookName">Name: ${name}</label>
        <label for="author">Author: ${author}</label>
        <label for="pages">Pages: ${pages}</label>
        <label for="haveRead">Have read?: ${read}</label>
        </div>
-       `}
+       `);
   }
+}
 
-const newBook = new Book([bookData])
-
-
-
+const newBook = new Book([bookData]);
 
 // const bookForm = document.getElementById('book-form')
 
 // bookForm.addEventListener('click', function(e){
 //     e.preventDefault()
-   
+
 //     const bookFormData = new FormData(bookForm)
 //     const bookName = bookFormData.get('bookName')
 //     const author = bookFormData.get('author')
@@ -90,4 +87,3 @@ const newBook = new Book([bookData])
 //        </div>
 //        `
 // }
-
