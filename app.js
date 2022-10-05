256c38d1e5716b65f181b9ad6ba99bf30431169d
kebin20 <ktanzyl@hotmail.com>
2022-10-05 21:14:26 +0900

    Added extra functions and updated code from cp

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // HTML Components
 const bookContainer = document.querySelector(".book-container");
+const overlay = document.querySelector(".overlay");
 
 //Book detail values
 const bookName = document.getElementById("bookName");
@@ -11,26 +12,38 @@ const haveRead = document.getElementById("haveRead");
 const addBookButton = document.querySelector(".add-book-button");
 const saveBookButton = document.querySelector(".save-book-button");
 const bookModal = document.querySelector(".book-modal");
-const clearBookDetails = document.querySelector(".clear-button");
+const clearDetailsButton = document.querySelector(".clear-button");
 
 //Render the add book screen
 let bookScreenOn = false;
 
 addBookButton.addEventListener("click", renderAddBookScreen);
 
-function renderAddBookScreen() {
-  bookScreenOn = true;
-  bookModal.classList.remove("hidden");
-  bookModal.classList.add("visible");
-}
-
 saveBookButton.addEventListener("click", () => {
   addBookData();
   bookScreenOn = false;
   bookModal.classList.remove("visible");
   bookModal.classList.add("hidden");
+  overlay.style.display = "none";
   bookData[bookData.length - 1].showBookDataHtml(bookData);
+  clearDetails();
 });
+
+clearDetailsButton.addEventListener("click", clearDetails);
+
+function clearDetails() {
+  bookName.value = "";
+  author.value = "";
+  pages.value = "";
+  haveRead.value = "";
+}
+
+function renderAddBookScreen() {
+  bookScreenOn = true;
+  bookModal.classList.remove("hidden");
+  bookModal.classList.add("visible");
+  overlay.style.display = "flex";
+}
 
 //Push new inputed books into bookData array
 function addBookData() {
@@ -57,16 +70,24 @@ class Book {
   showBookDataHtml(data) {
     const { name, author, pages, read } = this;
     for (let book of bookData) {
+      // bookData.map((book) => {
       return (bookContainer.innerHTML += `
-         <div class="book" id="showBook">
-         <label for="bookName">Name: ${name}</label>
-         <label for="author">Author: ${author}</label>
-         <label for="pages">Pages: ${pages}</label>
-         <label for="haveRead">Have read?: ${read}</label>
-         </div>
-         `);
+       <div class="book" id="showBook">
+       <label for="bookName">Name: ${name}</label>
+       <label for="author">Author: ${author}</label>
+       <label for="pages">Pages: ${pages}</label>
+       <label for="haveRead">Have read?: ${read}</label>
+       </div>
+       `);
     }
   }
 }
 
-const newBook = new Book([bookData]);
+//Modal Function
+const closeButton = document.getElementById("modal-close-btn");
+
+closeButton.addEventListener("click", () => {
+  bookModal.classList.remove("visible");
+  bookModal.classList.add("hidden");
+  overlay.style.display = "none";
+});
